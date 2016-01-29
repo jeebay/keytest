4fca97cc351f2cb6ff4d4ced9031baa3b7c08d1d
James Leisy <jamesleisy@gmail.com>
2016-01-29 18:18:11 -0500

    added image & meter logic

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+console.log('linked');
+
 (function() {
   var state = {
     k1: 0,
@@ -8,24 +10,45 @@
 
   var p1meter = document.querySelector('meter.meter1');
   var p2meter = document.querySelector('meter.meter2');
+  var p1img = document.querySelector('.player_1_img');
+  var p2img = document.querySelector('.player_2_img');
+
+  function initialize(){
+  	p1img.style.backgroundImage = "url(img/goku1.jpg)";
+  	p2img.style.backgroundImage = "url(img/veg1.jpg)";
+  }
 
   function render() {
     p1meter.value = state.p1.toString();
     p2meter.value = state.p2.toString();
+
+ 
+    if (state.p1 >= 0.5 && state.p1 < 1){
+    	p1img.style.backgroundImage = "url(img/goku2.jpg)";
+    }else if (state.p1 >= 1){
+    	p1img.style.backgroundImage = "url(img/goku3.jpg)";
+    }
+
+    if (state.p2 >= 0.5 && state.p2 < 1){
+    	p2img.style.backgroundImage = "url(img/veg2.jpg)";
+    }else if (state.p2 >= 1){
+    	p2img.style.backgroundImage = "url(img/veg3.jpg)";
+    }
   }
 
   function keyPress (e) {
-    if (e === 65 || e === 68) {
-      if (e !== state.k1) {
-        state.k1 = e;
-        state.p1 += 0.1;
+    var key = e.keyCode;
+    if (key === 65 || key === 68) {
+      if (key !== state.k1) {
+        state.k1 = key;
+        state.p1 += 0.01;
       }
     }
 
-    if (e === 37 || e === 39) {
-      if (e !== state.k2) {
-        state.k2 = e;
-        state.p2 += 0.1;
+    if (key === 37 || key === 39) {
+      if (key !== state.k2) {
+        state.k2 = key;
+        state.p2 += 0.01;
       }
     }
 
@@ -33,4 +56,5 @@
   }
 
   document.body.addEventListener('keyup', keyPress);
+  initialize();
 })();
